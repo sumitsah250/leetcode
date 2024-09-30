c3ff94c12021c4d91205230f5f61152b12ed3321
sumit <sumitsah250@gmail.com>
2024-09-30 16:59:05 +0545

    just find the first bad verstion

---
+++ q6_first_bad_version.cpp
@@ -0,0 +1,15 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        for(int i=1;i<=n;i++){
+            if(isBadVersion(i)==true){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+};
